04aad2b82730ec53a24a6887972a336a269e1f28
Renzo Rosales <rosalesponcer@gmail.com>
2022-03-21 16:58:20 -0500

    update

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -1,18 +1,50 @@
 
 import Project from '../models/Project';
 
+export const getAllProjects = async (req, res) => {
+    try {
+        const findedProject = await Project.find({}).exec();
+
+        res.status(200).json(findedProject);
+
+    } catch (error) {
+        res.status(400).json(error);
+    }
+}
+
 export const getProject = async (req, res) => {
-    const { id } = req.params;
 
-    const savedProject = await Project.findById(id).exec();
+    try {
+        const { id } = req.params;
 
-    res.status(200).json(savedProject);
+        const savedProject = await Project.findById(id).exec();
+
+        res.status(200).json(savedProject);
+    } catch (error) {
+        res.status(400).json(error);
+    }
 }
 
 export const saveProject = async (req, res) => {
-    const { name, scenes } = req.body;
 
-    const savedProject = await Project.create({ name, scenes })
+    try {
+        const { name, scenes } = req.body;
 
-    res.status(200).json(savedProject);
+        const savedProject = await Project.create({ name, scenes });
+
+        res.status(200).json(savedProject);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+}
+
+export const updateProject = async (req, res) => {
+
+    try {
+        const savedProject = await Project.findByIdAndUpdate(req.params?.id, req.body);
+
+        res.status(200).json(savedProject);
+    } catch (error) {
+        res.status(400).json(error);
+    }
 }
--- src/routers/project.routes.js
+++ src/routers/project.routes.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 
-import { getProject, saveProject } from '../controllers/project.controller';
+import { getProject, saveProject, updateProject, getAllProjects } from '../controllers/project.controller';
 
 const router = Router();
 
 const END_POINT = '/project';
 
+router.get(`${END_POINT}`, getAllProjects);
 router.get(`${END_POINT}/:id`, getProject);
 router.post(`${END_POINT}`, saveProject);
-
+router.put(`${END_POINT}/:id`, updateProject);
 
 export default router;
