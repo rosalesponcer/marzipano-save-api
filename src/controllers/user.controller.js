910198100f73aab653ab4d283d3c38def9829b53
Renzo Rosales <rosalesponcer@gmail.com>
2022-03-25 16:54:21 -0500

    update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ export const login = (req, res) => {
     try {
         const { user, pass } = req.body;
 
-        if (user === 'picnic' && pass === 'picnic321') res.status(200).json({ acces: true })
+        if (user === 'picnic' && pass === 'picnic321') res.status(200).json({ access: true })
         else res.status(400).json({ access: false })
 
     } catch (error) {
